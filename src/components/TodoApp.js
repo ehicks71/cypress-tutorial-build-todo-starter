f0b8d7aadb1b60d1968d74d907323266176bbe3d
Elizabeth <hicksem@wfu.edu>
2023-11-27 17:37:42 -0500

    Adding tests and code for filters

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -4,7 +4,7 @@ import TodoForm from './TodoForm'
 import TodoList from './TodoList'
 import Footer from './Footer'
 import {saveTodo, loadTodos, destroyTodo, updateTodo} from '../lib/service'
-
+import {filterTodos} from '../lib/utils'
 
 export default class TodoApp extends Component {
   constructor(props) {
@@ -77,9 +77,12 @@ export default class TodoApp extends Component {
               handleNewTodoChange={this.handleNewTodoChange} />
           </header>
           <section className="main">
-            <TodoList todos={this.state.todos}
+          <Route path='/:filter?' render={({match}) =>
+            <TodoList
+              todos={filterTodos(match.params.filter, this.state.todos)}
               handleDelete={this.handleDelete}
               handleToggle={this.handleToggle} />
+            } />
           </section>
           <Footer remaining={remaining} />
         </div>
