49f050fbc2a028d0428d09de003e693573dcf92f
Elizabeth <hicksem@wfu.edu>
2023-11-27 17:27:45 -0500

    Tests and code for updating todo singular and plural

---
+++ cypress/integration/footer.spec.js
@@ -0,0 +1,20 @@
+describe('Footer', () => {
+  context('with a single todo', () => {
+    it('displays a singular todo in count', () => {
+      cy.seedAndVisit([{id: 1, name: 'Buy milk', isComplete: false}])
+      cy.get('.todo-count')
+        .should('contain', '1 todo left')
+    });
+  });
+
+  context('with multiple todos', () => {
+    beforeEach(() => {
+      cy.seedAndVisit()
+    });
+
+    it('displays plural todos in count', () => {
+      cy.get('.todo-count')
+        .should('contain', '3 todos left')
+    });
+  });
+});
